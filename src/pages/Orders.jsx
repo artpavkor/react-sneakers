0cdee21b76239d65363b15acf64428f0885377b3
Artem Korniienko <artpavkor@gmail.com>
2023-04-22 21:02:28 +0300

    fix orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,6 +5,7 @@ import OrdersCart from '../components/OrdersCart';
 function Orders() {
   const [isLoading, setIsLoading] = useState(true);
   const [orders, setOrders] = useState([]);
+
   useEffect(() => {
     (async () => {
       try {
@@ -21,10 +22,28 @@ function Orders() {
       }
     })();
   }, []);
+
+  const onRemoveOrd = async (id) => {
+    try {
+      setOrders([]);
+      await axios.delete(
+        `https://641d8e42945125fff3d0159f.mockapi.io/orders/${id}`
+      );
+    } catch (error) {}
+  };
+  function deleteCardFav() {
+    orders.map((item) => {
+      return onRemoveOrd(item.id);
+    });
+  }
+
   return (
     <div className="content p-40">
       <div className="d-flex align-center mb-40 justify-between">
         <h1>Мои заказы</h1>
+        <button onClick={deleteCardFav} className="greenButton_favorite">
+          Удалить
+        </button>
       </div>
       <div className="orders_row">
         {(isLoading ? [...Array(5)] : orders).map((item, index) => {
